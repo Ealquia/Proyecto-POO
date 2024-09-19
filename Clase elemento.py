af50a9d0a911113e7804005b1fbce3ea9bcbe5f4
isaac1525 <isaacgarciadiaz2@gmail.com>
2024-09-18 22:57:48 -0600

    Update Clase elemento.py

--- Clase elemento.py
+++ Clase elemento.py
@@ -1,21 +1,22 @@
 import pandas as pd
 
 class Elemento:
-    # Cargar el CSV de los elementos en un DataFrame (atributo estático de la clase)
-    elementos_df = pd.read_csv(r'C:/Users/isaac/Downloads/tabla CSV/c2dd862cd38f21b0ad36b8f96b4bf1ee-1d92663004489a5b6926e944c1b3d9ec5c40900e/Periodic Table of Elements.csv')
+    try:
+        elementos_df = pd.read_csv(r'C:/Users/isaac/Downloads/tabla CSV/c2dd862cd38f21b0ad36b8f96b4bf1ee-1d92663004489a5b6926e944c1b3d9ec5c40900e/Periodic Table of Elements.csv')
+    except FileNotFoundError:
+        raise FileNotFoundError("El archivo de la tabla periódica no se encontró.")
 
     def __init__(self, simbolo):
-        # Buscamos el elemento en el dataframe
-        elemento_info = self.elementos_df[self.elementos_df['Symbol'] == simbolo]
+        elemento_info = Elemento.elementos_df[Elemento.elementos_df['Symbol'] == simbolo]
         
         if not elemento_info.empty:
             # Asigna los atributos al elemento
             self.elemento = elemento_info['Element'].values[0]
-            self.cant = 0  # Cantidad inicial (puede modificarse)
+            self.cant = 0 
             self.masa_molar = elemento_info['AtomicMass'].values[0]
             self.densidad = elemento_info['Density'].values[0]
             self.nombre = elemento_info['Element'].values[0]
-            self.otros = elemento_info.to_dict(orient='records')[0]  # Guarda otros datos como diccionario
+            self.otros = elemento_info.to_dict(orient='records')[0] 
         else:
             raise ValueError(f"El elemento con símbolo '{simbolo}' no fue encontrado en la tabla periódica.")
 
@@ -35,14 +36,12 @@ class Elemento:
         return self.densidad
 
     def toString(self):
-        # Devuelve una lista cono toda la informacion
         return f"Elemento: {self.nombre}, Masa Molar: {self.masa_molar}, Densidad: {self.densidad}, Otros: {self.otros}"
 
-    
+    @classmethod
     def iniciar_interaccion(cls):
         seguir = True
         while seguir:
-            # Pide al usuario el símbolo del elemento
             simbolo = input("Introduce el símbolo del elemento (o escribe 'salir' para terminar): ")
             if simbolo.lower() == 'salir':
                 seguir = False
@@ -50,8 +49,8 @@ class Elemento:
             else:
                 try:
                     elemento = cls(simbolo)
-                    # Imprimir la información del elemento
                     print(elemento.toString())
                 except ValueError as e:
                     print(e)
+
 Elemento.iniciar_interaccion()
